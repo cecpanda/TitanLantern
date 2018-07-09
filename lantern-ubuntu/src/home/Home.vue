92b6206a0d40551ac0bff6016c9d3199fff8c104
Duncan <man_hattan@qq.com>
2018-07-09 17:15:35 +0800

    start frontend for ubuntu

---
+++ lantern-ubuntu/src/home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='home'>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+
+</script>
+
+<style lang='stylus' scoped>
+.el-header
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+.el-main
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+</style>
